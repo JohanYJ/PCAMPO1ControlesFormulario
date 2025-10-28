ae7d80e2cc9315dcdd6c22950bd6d0cb0ed5ab03
Johan <Johan@JOHAN-PRINCIPAL>
2025-10-27 23:43:58 -0500

    Agregar formulario con ComboBox para seleccionar carrera

---
+++ PCAMPO1ControlesFormulario/FormularioCarrera.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PCAMPO1ControlesFormulario
+{
+    partial class FormularioCarrera
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbCarrera = new System.Windows.Forms.ComboBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.lblSeleccion = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seleccione su carrera:";
+            // 
+            // cmbCarrera
+            // 
+            this.cmbCarrera.FormattingEnabled = true;
+            this.cmbCarrera.Location = new System.Drawing.Point(165, 41);
+            this.cmbCarrera.Name = "cmbCarrera";
+            this.cmbCarrera.Size = new System.Drawing.Size(121, 21);
+            this.cmbCarrera.TabIndex = 1;
+            // 
+            // btnConfirmar
+            // 
+            this.btnConfirmar.Location = new System.Drawing.Point(117, 86);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(75, 23);
+            this.btnConfirmar.TabIndex = 2;
+            this.btnConfirmar.Text = "Confirmar";
+            this.btnConfirmar.UseVisualStyleBackColor = true;
+            this.btnConfirmar.Click += new System.EventHandler(this.btnConfirmar_Click);
+            // 
+            // lblSeleccion
+            // 
+            this.lblSeleccion.AutoSize = true;
+            this.lblSeleccion.Location = new System.Drawing.Point(105, 128);
+            this.lblSeleccion.Name = "lblSeleccion";
+            this.lblSeleccion.Size = new System.Drawing.Size(0, 13);
+            this.lblSeleccion.TabIndex = 3;
+            // 
+            // FormularioCarrera
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 262);
+            this.Controls.Add(this.lblSeleccion);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.cmbCarrera);
+            this.Controls.Add(this.label1);
+            this.Name = "FormularioCarrera";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormularioCarrera_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbCarrera;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.Label lblSeleccion;
+    }
+}
+
---
+++ PCAMPO1ControlesFormulario/FormularioCarrera.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PCAMPO1ControlesFormulario
+{
+    public partial class FormularioCarrera : Form
+    {
+        public FormularioCarrera()
+        {
+            InitializeComponent();
+        }
+
+        private void FormularioCarrera_Load(object sender, EventArgs e)
+        {
+            cmbCarrera.Items.AddRange(new string[]
+            {
+             "Ingeniería",
+             "Medicina",
+             "Derecho",
+             "Arquitectura"
+            });
+        }
+
+        private void btnConfirmar_Click(object sender, EventArgs e)
+        {
+            string carrera = cmbCarrera.SelectedItem?.ToString();
+            lblSeleccion.Text = $"Has elegido: {carrera}";
+        }
+    }
+}
